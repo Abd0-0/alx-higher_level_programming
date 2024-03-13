6e0e4a50a712948e72bce3d818e69bb4970086e1
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2024-03-12 20:01:30 -0700

    task13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -34,13 +34,6 @@ listint_t *insert_node(listint_t **head, int number)
 
 	ptr = *head;
 
-	if (ptr->n < number)
-	{
-		new->next = ptr;
-		*head = new;
-		return (new);
-	}
-
 	while (ptr && ptr->next)
 	{
 		if (ptr->n < number && ptr->next->n > number)
