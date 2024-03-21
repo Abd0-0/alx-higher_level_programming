38f69ef0dab6eac18d6be90c8c4b4f7c5faf1b9a
Abd0_0 <abdalrahman.magdy2000@gmail.com>
2024-03-20 19:57:56 -0700

    task 9

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary:
         best_s = max(a_dictionary, key=a_dictionary.get)
-    return best_S
+        return best_S
+    else:
+        return None
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {a_dictionary[k] ** 2 fork in a_dictionary}
+    return {a_dictionary[k] * 2 fork in a_dictionary}
